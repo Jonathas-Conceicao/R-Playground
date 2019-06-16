384e23f9ae3dccd805d00b246f1da9bcc2a2ff2d
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2019-06-15 23:40:00 -0300

    Clean csv data from tables
    Signed-off-by: Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>

--- trab1/process.R
+++ trab1/process.R
@@ -1,7 +1,20 @@
 #! /usr/bin/Rscript
 library(ggplot2)
 
-process <- function(file = "raw_data.csv") {
-  table <- read.csv(file, header=FALSE)
+process <- function(file = "clean_data.csv") {
+  table <- read.csv(file, header=TRUE)
   table
+}
+
+graph_fn <- function(df, col=1) {
+  df <- data.frame(df[,col])
+  names(df) <- c("V1")
+  ggplot(df, aes(x=V1)) +
+  scale_y_continuous(name = "Frequência") +
+  geom_histogram(stat="count")
+}
+
+t <- process()
+for (col in 1:11) {
+  print(graph_fn(t, col))
 }
