0a020824727d1b3617d7e287cb92b1212b835320
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2019-06-16 15:51:30 -0300

    trab1: Update
    - Inverted Axis on a graph for better visualization of X values;
    - Better graph functions;
    - Exact axis values on top of bars;
    
    Signed-off-by: Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>

--- trab1/process.R
+++ trab1/process.R
@@ -6,15 +6,66 @@ process <- function(file = "clean_data.csv") {
   table
 }
 
-graph_fn <- function(df, col=1) {
-  df <- data.frame(df[,col])
+bar_graph <- function(df,
+                     col=1,
+                     title="Titulo",
+                     x="Eixo X",
+                     y="Contagem",
+                     conf=labs(),
+                     conf2=aes()) {
+  df <- data.frame(sort(df[,col]))
   names(df) <- c("V1")
   ggplot(df, aes(x=V1)) +
-  scale_y_continuous(name = "Frequência") +
-  geom_histogram(stat="count")
+    labs(title = title) +
+    labs(x = x) +
+    labs(y = y) +
+    conf +
+    ## labs(colour = "Implementation") +
+    geom_bar(conf2, position = "dodge") +
+    geom_text(
+        aes(label=..count..),
+        stat="count",
+        position = position_dodge(0.9),
+        vjust = 0
+    )
 }
 
 t <- process()
-for (col in 1:11) {
-  print(graph_fn(t, col))
-}
+bar_graph(t,
+         1,
+         "Curso",
+         "",
+         conf=coord_flip())
+bar_graph(t,
+         2,
+         "Ingresso",
+         "ano/semestre")
+bar_graph(t,
+         3,
+         "Consome Álcool",
+         "")
+bar_graph(t,
+         4,
+         "Pratica Atividade Física",
+         "")
+bar_graph(t,
+         6,
+         "Tem alguma prática espiritual, filosófica ou religião",
+         "")
+bar_graph(t,
+         8,
+         "Tempo médio de uso diário do smartphone em minutos",
+         "Número de horas")
+bar_graph(t,
+         9,
+         "Número de disciplinas cursadas",
+         "")
+bar_graph(t,
+         10,
+         "Já pensou em desistir do curso",
+         "")
+bar_graph(t,
+         11,
+         "Satisfação com a estrutura da UFPel",
+         "Nível")
+
